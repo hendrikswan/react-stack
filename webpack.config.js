e394fa63e699e155984d5b6964e65fe2fbb309a1
Emmanuel Morales <Emmanuel Morales>
2015-12-17 11:55:08 -0600

    Add bootstrap-sass-loader and upgrade dependencies for bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,15 @@ module.exports = {
         test: /\.scss$/,
         include: path.join(__dirname, 'src'),
         loader: 'style!css!sass'
-      }
+      },
+
+      // Needed for the css-loader when [bootstrap-webpack](https://github.com/bline/bootstrap-webpack)
+      // loads bootstrap's css.
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" }
     ]
   }
 }
