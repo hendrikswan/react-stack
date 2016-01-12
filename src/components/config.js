ac836ccf471febf651ab999a243763f38a8beaac
Emmanuel Morales <emmanuel.morales@headspring.com>
2016-01-12 17:14:07 -0600

    Additional clean up

--- src/components/config.js
+++
@@ -1,5 +0,0 @@
-/*eslint no-process-env:0*/
-export default {
-  port: process.env.port || 3002
-  ,env: process.env.NODE_ENV || 'development'
-};
--- src/components/index.js
+++ src/components/index.js
@@ -1,51 +1,11 @@
 import BaseComponent from './BaseComponent.jsx';
-// import CollapsibleList from './CollapsibleList.jsx';
-// import ConfirmationModal from './ConfirmationModal.jsx';
-// import EditableText from './EditableText.jsx';
-// import InlineTextEdit from './InlineTextEdit.jsx';
-// import InputList from './InputList.jsx';
-// import InputListItem from './InputListItem.jsx';
-// import Nav from './Nav.jsx';
-// import ShowMoreButton from './ShowMoreButton.jsx';
-// import Sidebar from './Sidebar.jsx';
-// import Spinner from './Spinner.jsx';
-// import StaticField from './StaticField.jsx';
-// import Toastr from './Toastr.jsx';
 import HttpLink from './HttpLink.jsx';
 import EmailLink from './EmailLink.jsx';
 import PhoneLink from './PhoneLink.jsx';
 
-// import Actions from './actions';
-// import Constants from './constants';
-// import Stores from './stores';
-// import Mixins from './mixins';
-//import Utils from './utils';
-import Config from './config';
-
 export default {
 	BaseComponent
-  // ,CollapsibleList
-  // ,ConfirmationModal
-  // ,EditableText
-  // ,InlineTextEdit
-  // ,InputList
-  // ,InputListItem
-  // ,Nav
-  // ,Panel
-  // ,ShowMoreButton
-  // ,Sidebar
-  // ,Spinner
-  // ,StaticField
-  // ,Toastr
   ,HttpLink
   ,EmailLink
   ,PhoneLink
-
-  // ,Action: Actions
-  // ,Constant: Constants
-  // ,Store: Stores
-  // ,Mixin: Mixins
-  // ,Util: Utils
-
-  ,Config
 };
